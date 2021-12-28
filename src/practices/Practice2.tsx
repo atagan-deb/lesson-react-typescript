775793f5843f39e8a251e92c6d50f216a377e942
atagan-deb <96320896+atagan-deb@users.noreply.github.com>
2021-12-29 07:21:33 +0900

    練習問題２ 返却値の型指定

---
+++ src/practices/Practice2.tsx
@@ -0,0 +1,12 @@
+export const Practice2 = () => {
+  const getTotalFee = (num: number): number => {
+    return num * 1.1;
+  };
+  const onClickPractice = () => console.log(getTotalFee(1000));
+  return (
+    <div>
+      <p>練習問題：返却値の型指定</p>
+      <button onClick={onClickPractice}>練習問題２を実行</button>
+    </div>
+  );
+};
